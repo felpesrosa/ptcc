0a8421965db685142420017f2ebb16da956b4272
felpesrosa <felpesrosa@gmail.com>
2022-12-06 23:27:00 -0300

    ajustes arquivo bash

--- vegeta-config/_test-runner.sh
+++ vegeta-config/_test-runner.sh
@@ -1,39 +1,39 @@
 # script para rodar testes com vegeta
 
 # total de 500 requisições em 5s com PlatformThreads
-echo ">> rodando teste 1"
+echo "inicio teste 1>" $(date)
 vegeta attack -duration=5s -rate=100 -targets=1-Target_with_Platform_Threads.txt | vegeta report > ./results/results-test-1.txt
-echo "fim teste 1"
+echo "fim teste 1>" $(date)
 
 sleep 120s
 # total de 500 requisições em 5s com SingleThread
-echo ">> rodando teste 2"
+echo "inicio teste 2>" $(date)
 vegeta attack -duration=5s -rate=100 -targets=2-Target_with_Single_Thread.txt | vegeta report > ./results/results-test-2.txt
-echo "fim teste 2"
+echo "fim teste 2>" $(date)
 
 sleep 120s
 # total de 500 requisições em 5s com CachedThreads
-echo ">> rodando teste 3"
+echo "inicio teste 3>" $(date)
 vegeta attack -duration=5s -rate=100 -targets=3-Target_with_Cached_Threads.txt | vegeta report > ./results/results-test-3.txt
-echo "fim teste 3"
+echo "fim teste 3>" $(date)
 
 sleep 120s
 # total de 500 requisições em 5s com VirtualThreads
-echo ">> rodando teste 4"
+echo "inicio teste 4>" $(date)
 vegeta attack -duration=5s -rate=100 -targets=4-Target_with_Virtual_Threads.txt | vegeta report > ./results/results-test-4.txt
-echo "fim teste 4"
+echo "fim teste 4>" $(date)
 
 sleep 120s
 # total de 500 requisições em 5s com CachedThread e com delay
-echo ">> rodando teste 5"
+echo "inicio teste 5>" $(date)
 vegeta attack -duration=5s -rate=100 -targets=5-Target_with_Cached_Threads_Delay.txt | vegeta report > ./results/results-test-5.txt
-echo "fim teste 5"
+echo "fim teste 5>" $(date)
 
 sleep 120s
 # total de 500 requisições em 5s com VirtualThreads e com delay
-echo ">> rodando teste 6"
+echo "inicio teste 6>" $(date)
 vegeta attack -duration=5s -rate=100 -targets=6-Target_with_Virtual_Threads_Delay.txt | vegeta report > ./results/results-test-6.txt
-echo "fim teste 6"
+echo "fim teste 6>" $(date)
 
 
 echo ">>>>>>>>>> fim <<<<<<<<<<"
